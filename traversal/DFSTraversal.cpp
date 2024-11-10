aa4cd2b8602fbcfef0f5305e21ce11970a057401
Aniket Chugh <149312276+Aniket-Chugh@users.noreply.github.com>
2024-11-10 14:04:40 +0530

    Update DFSTraversal.cpp

--- traversal/DFSTraversal.cpp
+++ traversal/DFSTraversal.cpp
@@ -1,65 +1,48 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-
 using namespace std;
 
-// BFS Traversal function
-vector<int> BfsTraversal(int V, vector<int> adj[])
+void DFS(int node, vector<int> adj[], vector<int> &ans, vector<bool> &visited)
 {
-    queue<int> q;
-    vector<bool> visited(V, 0);
-    q.push(0);
-    visited[0] = 1;
-    vector<int> ans;
-
-    int node;
-
-    while (!q.empty())
+    visited[node] = 1;
+    ans.push_back(node);
+    for (int j = 0; j < adj[node].size(); j++)
     {
-        node = q.front();
-        q.pop();
-
-        ans.push_back(node);
-
-        for (int j = 0; j < adj[node].size(); j++)
+        if (!visited[adj[node][j]])
         {
-            if (!visited[adj[node][j]])
-            {
-                visited[adj[node][j]] = 1;
-                q.push(adj[node][j]);
-            }
+            DFS(adj[node][j], adj, ans, visited);
         }
     }
+}
+
+vector<int> dfsTra(int V, vector<int> adj[])
+{
+    vector<bool> visited(V, 0);
+    vector<int> ans;
+    DFS(0, adj, ans, visited);
     return ans;
 }
 
 int main()
 {
-    int V, E; // Number of vertices and edges
-    cout << "Enter number of vertices and edges: ";
+    int V, E;
     cin >> V >> E;
-
-    // Adjacency list representation
     vector<int> adj[V];
+    int u, v;
 
-    cout << "Enter each edge as two vertex indices (e.g., 0 1 for an edge between vertex 0 and 1):" << endl;
+    // Use E as the loop counter for edges
     for (int i = 0; i < E; i++)
     {
-        int u, v;
         cin >> u >> v;
         adj[u].push_back(v);
-        adj[v].push_back(u); // Assuming an undirected graph
+        adj[v].push_back(u);
     }
 
-    // Perform BFS traversal from node 0
-    vector<int> bfsResult = BfsTraversal(V, adj);
+    vector<int> dfstra = dfsTra(V, adj);
 
-    // Output the BFS traversal result
-    cout << "BFS Traversal starting from node 0: ";
-    for (int i = 0; i < bfsResult.size(); i++)
+    for (int i = 0; i < dfstra.size(); i++)
     {
-        cout << bfsResult[i] << " ";
+        cout << dfstra[i] << " ";
     }
     cout << endl;
 
